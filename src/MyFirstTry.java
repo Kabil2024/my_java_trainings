553ebaa7adf5befdefb5bc5e76b4f0d47d1d5089
Kabil2024 <kabilarasan44@gmail.com>
2024-12-26 03:55:58 -0800

    newfile

---
+++ src/MyFirstTry.java
@@ -0,0 +1,16 @@
+public  class MyFirstTry {
+    public static void main(String[] args){
+        String name = "Kabil Arasan";
+        int age = 25;
+        float height = 5.9f;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+        String secName = "GokJay";
+        int secAge = 21;
+        float secHeight = 5.5f;
+        System.out.println("Name: " + secName);
+        System.out.println("Age: " + secAge);
+        System.out.println("Height "+ secHeight + " feet");
+    }
+}
---
+++ src/MyProgram.java
@@ -0,0 +1,8 @@
+public class MyProgram {
+    public static void main( String[] args ){
+        String name = "kabilarasan";
+        int a = 44;
+        System.out.println(name + a);
+
+    }
+}
